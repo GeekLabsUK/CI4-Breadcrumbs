97dc2802ff48e1a130fc567f4238bdda3a63d1d1
Geeklabs <geeklabsonline@gmail.com>
2020-11-19 00:43:25 +0000

    Update Breadcrumb.php

--- Breadcrumb.php
+++ Breadcrumb.php
@@ -1,36 +1,57 @@
-<?php 
+<?php
+
+/**
+ * --------------------------------------------------------------------
+ * CODEIGNITER 4 - CI4 Breadcrumbs
+ * --------------------------------------------------------------------
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * @package    CI4 Breadcrumbs
+ * @author     GeekLabs - Lee Skelding 
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://github.com/GeekLabsUK/CI4-Breadcrumbs
+ * @since      Version 2.0
+ * 
+ */
 
 namespace App\libraries;
 
-class Breadcrumb {
+//use CodeIgniter\HTTP\URI;
+
+class Breadcrumb
+{
 
     private $breadcrumbs = array();
     private $tags;
 
-    public function __construct(){
+    public function __construct()
+    {
+        $this->URI = service('uri');
+
+        // SHOULD THE LAST BREADCRUMB BE A CLICKABLE LINK? iF SO SET TO TRUE
+        $this->clickable = true;
 
         // create our bootstrap html elements
-
         $this->tags['navopen']  = "<nav aria-label=\"breadcrumb\">";
         $this->tags['navclose'] = "</nav>";
         $this->tags['olopen']   = "<ol class=\"breadcrumb\">";
         $this->tags['olclose']  = "</ol>";
         $this->tags['liopen']   = "<li class=\"breadcrumb-item\">";
         $this->tags['liclose']  = "</li>";
-       
     }
 
-    public function add($crumb, $href){
+    public function add($crumb, $href)
+    {
 
-        if(!$crumb OR !$href) return; // if the title or Href not set return 
+        if (!$crumb or !$href) return; // if the title or Href not set return 
 
         $this->breadcrumbs[] = array(
             'crumb' => $crumb,
             'href' => $href,
         );
-
     }
-   
+
 
     public function render()
     {
@@ -61,8 +82,49 @@ class Breadcrumb {
         return $output;
     }
 
-    
-        
-    
+    public function buildAuto()
+    {
 
+        $urisegments = $this->URI->getSegments();
+
+        $output  = $this->tags['navopen'];
+        $output .= $this->tags['olopen'];
+
+        $crumbs = array_filter($urisegments);
+
+        $result = array();
+        $path = '';
+
+        // SUBTRACT 1 FROM COUNT IF THE LAST LINK IS TO NOT BE A LINK
+        $count = count($crumbs) ;
+
+        if ($this->clickable){
+
+            $count = count($crumbs) -1;
+        }
+        
+
+        foreach ($crumbs as $k => $crumb) {
+
+            $path .= '/' . $crumb;
+
+            $name = ucfirst(str_replace(array(".php", "_"), array("", " "), $crumb));
+
+            if ($k != $count) {
+
+                $result[] = $this->tags['liopen'] . '<a href="' . $path . '"> ' . $name . '</a>' . $this->tags['liclose'];
+
+            } else {
+
+                $result[] = $this->tags['liopen'] . $name . $this->tags['liclose'];
+
+            }
+        }
+
+        $output .= implode($result);
+        $output .= $this->tags['olclose'];
+        $output .= $this->tags['navclose'];
+
+        return $output;
+    }
 }
